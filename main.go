bf128fb00790ce7f8ad47ed8545b6b088e85a4e7
姚威 <yaowei@yaoweideMacBook-Pro.local>
2021-09-24 15:52:05 +0800

    // 文章详情

--- main.go
+++ main.go
@@ -1,29 +1,41 @@
 package main
+
 //GO111MODULE=on go get -u github.com/cosmtrek/air
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func main() {
-	http.HandleFunc("/", handleFunc)
-	http.ListenAndServe(":3000", nil)
+	router := http.NewServeMux()
+	router.HandleFunc("/", defaultHandler)
+	router.HandleFunc("/about", aboutHandler)
+	// 文章详情
+	router.HandleFunc("/articles/", func(w http.ResponseWriter, r *http.Request) {
+		id := strings.SplitN(r.URL.Path, "/", 3)[2]
+		fmt.Fprint(w, "文章 ID："+id)
+	})
+
+	http.ListenAndServe(":3000", router)
 }
 
-func handleFunc(w http.ResponseWriter, r *http.Request) {
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	switch r.URL.Path {
-	case "/about":
-		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 Aron "+
-			"<a href=\"Aron.Yao@feisu.com\">251109226@qq.com</a>")
-		break
 	case "/":
 		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
 			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 		break
 	default:
 		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w,"<h1>请求页面未找到 :(</h1>")
+		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>")
 	}
 
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 Aron "+
+		"<a href=\"Aron.Yao@feisu.com\">251109226@qq.com</a>")
 }

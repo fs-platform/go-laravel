499425372c484557379b5e3ff7e7c551f0244a6d
姚威 <yaowei@yaoweideMacBook-Pro.local>
2021-09-24 15:14:24 +0800

    新增关于页面

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handleFunc)
+	http.ListenAndServe(":3000", nil)
+}
+
+func handleFunc(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/about":
+		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+			"<a href=\"Aron.Yao@feisu.com\">251109226@qq.com</a>")
+		break
+	case "/":
+		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+		break
+	default:
+		fmt.Fprint(w,"<h1>请求页面未找到 :(</h1>")
+	}
+
+}

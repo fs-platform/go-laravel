7df0264f512f765648fc134286e9b7fbf60525d6
姚威 <yaowei@yaoweideMacBook-Pro.local>
2021-09-27 18:17:08 +0800

    精简代码

--- main.go
+++ main.go
@@ -2,31 +2,14 @@ package main
 
 //GO111MODULE=on go get -u github.com/cosmtrek/air
 import (
-	"database/sql"
-	"fmt"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
 	"go_blog/app/http/middlewares"
 	"go_blog/bootstrap"
-	"go_blog/pkg/databases"
 	"net/http"
 )
 
-var db *sql.DB
-var router *mux.Router
-
 func main() {
-	databases.Initialize()
-	router = bootstrap.SetupRoute()
+	router := bootstrap.SetupRoute()
 	bootstrap.SetupDB()
-	db = databases.DB
 	//自定义404
-	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 	http.ListenAndServe(":3000", middlewares.RemoveSlash(router))
-}
-
-
-func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, "<h1>请求页面未找到 :(</h1><p>如有疑惑，请联系我们。</p>")
 }
--- routes/web.go
+++ routes/web.go
@@ -1,9 +1,11 @@
 package routes
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"go_blog/app/http/controllers"
 	"go_blog/app/http/middlewares"
+	"net/http"
 )
 
 func RegisterWebRouter(router *mux.Router) *mux.Router {
@@ -15,6 +17,12 @@ func RegisterWebRouter(router *mux.Router) *mux.Router {
 	router.HandleFunc("/articles/{id:[0-9]+}/edit", ArticleController.Edit).Methods("GET").Name("articles.edit")
 	router.HandleFunc("/articles/{id:[0-9]+}", ArticleController.Update).Methods("POST").Name("articles.update")
 	router.HandleFunc("/articles/{id:[0-9]+}/delete", ArticleController.Delete).Methods("GET").Name("articles.delete")
+	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 	router.Use(middlewares.SetHeaderMiddleware)
 	return router
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>请求页面未找到 :(</h1><p>如有疑惑，请联系我们。</p>")
 }

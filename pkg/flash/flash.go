8b4cb9064b99a01f663074873dcb17065ed6f7b9
姚威 <yaowei@yaoweideMacBook-Pro.local>
2021-09-30 09:53:18 +0800

    新增flash包

---
+++ pkg/flash/flash.go
@@ -0,0 +1,47 @@
+package flash
+
+import (
+	"encoding/gob"
+	"go_blog/pkg/session"
+)
+
+type flashes map[string]interface{}
+
+var flashKey string = "_flash"
+
+func init() {
+	gob.Register(flashes{})
+}
+
+func addFlash(key string, message string) {
+	flashes := flashes{}
+	flashes[key] = message
+	session.Put(flashKey, flashes)
+	session.Save()
+}
+
+func Info(message string) {
+	addFlash("info", message)
+}
+
+func Success(message string) {
+	addFlash("success", message)
+}
+
+func Danger(message string) {
+	addFlash("danger", message)
+}
+
+func Warning(message string) {
+	addFlash("warning", message)
+}
+
+func All() flashes {
+	flash := session.Get(flashKey)
+	flashMessage, err := flash.(flashes)
+	if !err {
+		return nil
+	}
+	defer session.Forget(flashKey)
+	return flashMessage
+}
